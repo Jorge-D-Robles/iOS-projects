7da89e8e8f423d6c0c434db883ead726f7e9c23a
Jorge Robles <robles8925@gmail.com>
2024-10-30 20:39:41 -0400

    Working on first iOS app

---
+++ project1/project1/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  project1
+//
+//  Created by Jorge Robles on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ project1/project1Tests/project1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  project1Tests.swift
+//  project1Tests
+//
+//  Created by Jorge Robles on 10/27/24.
+//
+
+import Testing
+@testable import project1
+
+struct project1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
