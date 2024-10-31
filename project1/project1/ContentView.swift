5fd50d4dd420e00c41503f29aad708f62101fdf0
Jorge Robles <robles8925@gmail.com>
2024-10-30 21:57:25 -0400

    added for each

--- project1/project1/ContentView.swift
+++ project1/project1/ContentView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
+            let emojis = ["👻", "👯‍♀️", "👩🏼‍🚀", "🐮", "😎"]
+            ForEach(emojis.indices, id: \.self) {
+                index in CardView(content: emojis[index])
+            }
+            
+            
+
         }
         .foregroundColor(.orange)
         .padding()
@@ -20,6 +24,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    let content: String
     @State var isFaceUp = false //temporary state
     
     var body: some View {
@@ -28,7 +33,7 @@ struct CardView: View {
             if isFaceUp {
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             }
             else {
                 base.fill()
